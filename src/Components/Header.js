c9a9e7be76d027f07b6959e5c29fd998ee1b5550
SHUBHAM BARAGI <145253243+shubhambaragi07@users.noreply.github.com>
2025-06-09 17:07:12 +0530

    first commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     if (this.props.data) {
-      var firstName = this.props.data.firstName
+      // var firstName = this.props.data.firstName
       var name = this.props.data.name
       var occupation = this.props.data.occupation
       var description = this.props.data.description
-      var city = this.props.data.address.city
+      // var city = this.props.data.address.city
       var networks = this.props.data.social.map(function (network) {
         return (
           <li key={network.name}>
